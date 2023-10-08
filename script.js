c213558f6f3a8ebab05be9ceb093ee9ef2c6f5e4
ZOMBOTRON <101598882+ZOMBOTRON@users.noreply.github.com>
2023-10-08 19:11:08 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = canvas.height = 50;
+
+function getColor() {
+  return Math.round(Math.random() * 100);
+}
+
+function generateNoise() {
+  for (let y = 0; y < canvas.height; y++) {
+    for (let x = 0; x < canvas.width; x++) {
+      const cor = getColor();
+      ctx.fillStyle = `hsl(0,0%,${cor}%)`;
+      ctx.fillRect(x, y, 1, 1);
+    }
+  }
+}
+
+generateNoise();
